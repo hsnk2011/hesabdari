14e1c51ba7ee9ca70d35b5c9e82e8768ae8dfc5d
hsnk2011 <124716618+hsnk2011@users.noreply.github.com>
2025-08-05 09:11:34 -0700

    Delete api/config.php

--- api/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Database configuration constants
-define('DB_SERVER', 'localhost');
-define('DB_NAME', 'arameshc_hesabdarisandbox');
-define('DB_USERNAME', 'arameshc_hesabdarisandbox');
-define('DB_PASSWORD', 'hesabdarisandbox');
-?>
